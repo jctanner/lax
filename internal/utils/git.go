77256c91d626920f8e0fbde81df33e54a06d6f3b
jctanner <tanner.jc@gmail.com>
2024-06-27 23:02:58 -0400

    use more go-git

--- internal/utils/git.go
+++ internal/utils/git.go
@@ -3,8 +3,7 @@ package utils
 import (
 	"fmt"
 	"os"
-	"os/exec"
-	"strings"
+	"time"
 
 	"github.com/go-git/go-git/v5" // with go modules disabled
 	"github.com/go-git/go-git/v5/plumbing"
@@ -57,6 +56,7 @@ func ListTags(path string) ([]string, error) {
 	return tags, nil
 }
 
+/*
 func GetCommitDate(repoPath string, commitHash string) (string, error) {
 	// Create the git command
 	cmd := exec.Command("git", "-C", repoPath, "show", "-s", "--format=%ci", commitHash)
@@ -71,7 +71,28 @@ func GetCommitDate(repoPath string, commitHash string) (string, error) {
 	date := strings.TrimSpace(string(output))
 	return date, nil
 }
+*/
 
+func GetCommitDate(repoPath string, commitHash string) (string, error) {
+	// Open the repository at the specified path
+	repo, err := git.PlainOpen(repoPath)
+	if err != nil {
+		return "", fmt.Errorf("failed to open repository: %v", err)
+	}
+
+	// Get the commit object using the commit hash
+	hash := plumbing.NewHash(commitHash)
+	commit, err := repo.CommitObject(hash)
+	if err != nil {
+		return "", fmt.Errorf("failed to get commit object: %v", err)
+	}
+
+	// Get the commit date
+	commitDate := commit.Committer.When
+	return commitDate.Format(time.RFC3339), nil
+}
+
+/*
 func GetLatestCommitHash(repoPath string) (string, error) {
 	// Create the git command
 	cmd := exec.Command("git", "-C", repoPath, "rev-parse", "HEAD")
@@ -86,7 +107,27 @@ func GetLatestCommitHash(repoPath string) (string, error) {
 	commitHash := strings.TrimSpace(string(output))
 	return commitHash, nil
 }
+*/
 
+func GetLatestCommitHash(repoPath string) (string, error) {
+	// Open the repository at the specified path
+	repo, err := git.PlainOpen(repoPath)
+	if err != nil {
+		return "", fmt.Errorf("failed to open repository: %v", err)
+	}
+
+	// Get the HEAD reference
+	headRef, err := repo.Head()
+	if err != nil {
+		return "", fmt.Errorf("failed to get HEAD reference: %v", err)
+	}
+
+	// Get the latest commit hash
+	commitHash := headRef.Hash().String()
+	return commitHash, nil
+}
+
+/*
 func CheckoutBranch(path, branchName string) error {
 	// Run the git checkout command as a subprocess with the working directory set to the repository path
 	cmd := exec.Command("git", "checkout", branchName)
@@ -96,7 +137,33 @@ func CheckoutBranch(path, branchName string) error {
 	}
 	return nil
 }
+*/
 
+func CheckoutBranch(path, branchName string) error {
+	// Open the repository at the specified path
+	repo, err := git.PlainOpen(path)
+	if err != nil {
+		return fmt.Errorf("failed to open repository: %v", err)
+	}
+
+	// Get the working tree of the repository
+	worktree, err := repo.Worktree()
+	if err != nil {
+		return fmt.Errorf("failed to get working tree: %v", err)
+	}
+
+	// Perform the checkout
+	err = worktree.Checkout(&git.CheckoutOptions{
+		Branch: plumbing.NewBranchReferenceName(branchName),
+	})
+	if err != nil {
+		return fmt.Errorf("failed to checkout branch: %v", err)
+	}
+
+	return nil
+}
+
+/*
 func CheckoutTag(path, tagName string) error {
 	// Run the git checkout command as a subprocess with the working directory set to the repository path
 	cmd := exec.Command("git", "checkout", tagName)
@@ -104,5 +171,42 @@ func CheckoutTag(path, tagName string) error {
 	if err := cmd.Run(); err != nil {
 		return fmt.Errorf("failed to checkout tag: %v", err)
 	}
+	return nil
+}
+*/
+
+func CheckoutTag(path, tagName string) error {
+	// Open the repository at the specified path
+	repo, err := git.PlainOpen(path)
+	if err != nil {
+		return fmt.Errorf("failed to open repository: %v", err)
+	}
+
+	// Get the tag reference
+	tagRef, err := repo.Tag(tagName)
+	if err != nil {
+		return fmt.Errorf("failed to get tag reference: %v", err)
+	}
+
+	// Get the commit hash for the tag
+	tagCommit, err := repo.CommitObject(tagRef.Hash())
+	if err != nil {
+		return fmt.Errorf("failed to get commit for tag: %v", err)
+	}
+
+	// Get the working tree of the repository
+	worktree, err := repo.Worktree()
+	if err != nil {
+		return fmt.Errorf("failed to get working tree: %v", err)
+	}
+
+	// Perform the checkout to the commit hash
+	err = worktree.Checkout(&git.CheckoutOptions{
+		Hash: tagCommit.Hash,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to checkout tag: %v", err)
+	}
+
 	return nil
 }
